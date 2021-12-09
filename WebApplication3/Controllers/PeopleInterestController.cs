7772a497ffdbc353d6ba644aa31fd75a872acc44
GET Academy <terje@kolderup.net>
2021-12-09 11:35:27 +0100

    Initial commit

---
+++ WebApplication3/Controllers/PeopleInterestController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using WebApplication3.DbModels;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleInterestController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<PersonInterest> Get()
+        {
+            var connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=InterestDb;Integrated Security=True";
+            var conn = new SqlConnection(connStr);
+
+            var id = new Guid("139913d1-3bd7-4016-974a-054e0f9a9151");
+            var sql = @"select p.Name Person, i.Name Interest
+                        from PersonInterest 
+                        join Interest i on InterestId = i.Id
+                        join Person p on PersonId = p.Id
+                        where PersonId = @Id";
+            var people = conn.Query<PersonInterest>(sql, new {Id = id});
+            return people;
+        }
+
+
+        [HttpPut]
+        public bool Terje(PersonInterest personInterest)
+        {
+            return true;
+
+        }
+    }
+}
---
+++ WebApplication3/DbModels/PersonInterest.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication3.DbModels
+{
+    public class PersonInterest
+    {
+        public string Person { get; set; }
+        public string Interest { get; set; }
+    }
+}
